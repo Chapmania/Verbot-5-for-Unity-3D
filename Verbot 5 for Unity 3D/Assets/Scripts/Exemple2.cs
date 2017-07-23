33d45f89a0cd6808580563f51bef5f450472017e
Casa-Pc\Casa-Main <jeffersonreis.pro@gmail.com>
2017-07-23 20:09:32 -0300

    - Prototype of the graphical interface;

---
+++ Verbot 5 for Unity 3D/Assets/Scripts/Exemple2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Exemple2 : ScriptTemplateForUI
+{
+    // Use this for initialization
+    void Start()
+    {
+        chatTextUI.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    /// <summary>
+    /// /// Send a message to UI
+    /// </summary>
+    public void SendMessageUI()
+    {
+        SendMessageUI("?????");
+    }
+
+
+}
---
+++ Verbot 5 for Unity 3D/Assets/Scripts/ScriptTemplateForUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ScriptTemplateForUI : MonoBehaviour {
+
+    public Text chatTextUI;
+    public InputField userInput;
+
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    /// <summary>
+    /// Send a message to UI
+    /// </summary>
+    /// <param name="botOutput"></param>
+    public void SendMessageUI(string botOutput)
+    {
+        //https://docs.unity3d.com/Manual/StyledText.html
+        chatTextUI.text += string.Format("<b>User</b>: {0}\n<b>Bot</b>: {1}\n\n", userInput.text, botOutput);
+        userInput.text = "";
+    }
+
+}
